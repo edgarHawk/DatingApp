b51372149fa96ac368d8edd6860a0bbf553df10a
Edgar Montoya <edgarhawk87@gmail.com>
2019-01-22 16:17:39 -0700

    Initial Commit

---
+++ Models/Value.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        internal int Id;
+
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
